c72363657a43c5e725b643652ad2a9967d5e73b3
Will Castillo <eduven@gmail.com>
2019-05-18 13:13:22 -0400

    Add files via upload
    initial commit

---
+++ src/content.js
@@ -0,0 +1,12 @@
+var titles = document.querySelectorAll("div.post-title > h2 > a");
+if (titles !== null) {
+    for (var i = 0, l = titles.length; i < l; i++) {
+      var title = titles[i].innerText;
+
+      title = title.replace(/[\xc2\xa1].*\!\s/i, "");
+      title = title.replace(/\([^)]*\)/, "");
+      title = title.replace(/\[[^)]*\]/, "");
+
+      titles[i].innerText = title;
+    }
+}
