95dc9f00a1237a6772c4a5c45f16842fb55b881c
Will Castillo <eduven@gmail.com>
2019-05-18 21:12:49 -0400

    Desensationalize post titles as well

--- src/content.js
+++ src/content.js
@@ -11,3 +11,17 @@ if (titles !== null) {
       titles[i].innerText = title;
     }
 }
+
+titles = document.querySelectorAll("div.post > h1");
+if (titles !== null) {
+    for (var i = 0, l = titles.length; i < l; i++) {
+      var title = titles[i].innerText;
+
+      title = title.replace(/[\xc2\xa1].*\!\s/i, "");
+      title = title.replace(/[\xc2\xbf].*\?\s/i, "");
+      title = title.replace(/\([^)]*\)/, "");
+      title = title.replace(/\[[^)]*\]/, "");
+
+      titles[i].innerText = title;
+    }
+}
